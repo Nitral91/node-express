39d9e8d456e8b82a78a47f4686fa0142e9321878
Pavel Nagornyuk <p.nagornyuk@cleack.com>
2020-07-05 22:30:24 +0300

    mocked new admin user

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 const homeRouter = require('./routes/home');
 const addRouter = require('./routes/add');
 const coursesRouter = require('./routes/courses');
-const cardRouter = require('./routes/cards');
+const cartRouter = require('./routes/cart');
+
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,12 +22,23 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5f02249ee7d5802cd89783a7');
+        req.user = user;
+        next() 
+    } catch(e) {
+        console.log(e);
+    }
+});
+
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRouter);
 app.use('/add', addRouter);
 app.use('/courses', coursesRouter);
-app.use('/card', cardRouter);
+app.use('/card', cartRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -40,7 +53,17 @@ async function start() {
             useFindAndModify: false,
             useUnifiedTopology: true
         });
-    
+        const candidate = await User.findOne();
+
+        if(!candidate) {
+            const user = new User({
+                email: 'admin@gmail.com',
+                name: 'Admin',
+                cart: {item: []}
+            });
+            await user.save();
+        }
+
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
